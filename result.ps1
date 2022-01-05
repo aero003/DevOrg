2676b692445f3d046973bfd54030a9be9e1180d9
aero003 <aeroadithiyan3@gmail.com>
2022-01-05 11:32:36 +0530

    update src

--- result.ps1
+++ result.ps1
@@ -1,39 +1,24 @@
-$output = Get-Content .\stdout.json | ConvertFrom-Json
-$componentSuccesses = @($output[0].result.details.componentSuccesses)
-$length = $componentSuccesses.Count
-#Write-Host "Length = $($length)"
-#$mailData = @()
-#$output[0].result.details.componentSuccesses[0] | Select-Object -Property componentType
-$mailData = @()
-for($i = 0; $i -lt $length; $i++)
-{
-    
-    $comType = $output[0].result.details.componentSuccesses[$i].componentType
-    if(($comType -eq "ApexClass") -or($comType -eq "ConnectedApp"))
-    {
-        $mailData += "************************************"
-        $mailData += "Component Type = $($output[0].result.details.componentSuccesses[$i].componentType)"
-        $mailData += "File Name = $($output[0].result.details.componentSuccesses[$i].fileName)"
-        $mailData += "Full Name = $($output[0].result.details.componentSuccesses[$i].fullName)"
-        $mailData +=  "Status = $($output[0].result.details.componentSuccesses[$i].success)" 
-        #Write-Host "Type = $($cmpType)"
-        Write-Host "Component Type = $($output[0].result.details.componentSuccesses[$i].componentType)" 
-        Write-Host "File Name = $($output[0].result.details.componentSuccesses[$i].fileName)" 
-        Write-Host "Full Name = $($output[0].result.details.componentSuccesses[$i].fullName)" 
-        Write-Host "Status = $($output[0].result.details.componentSuccesses[$i].success)" 
-        Write-Host "************************************"
-    }
-   
-}
-$mailData > .\mailResult.txt
-Write-Host "len = $($mailData.Count)"
+$JSONResult = Get-Content .\stdout.json | ConvertFrom-Json |Select-Object -expand  result |Select-Object -expand details | Select-Object -expand componentSuccesses 
+$JSONResult | Select-Object @{Name="Component Type";Expression={$_.componentType}}, @{Name="File Name";Expression={$_.fileName}}, @{Name="Full Name";Expression={$_.fullName}}, @{Name="Status";Expression={$_.success}} | Sort-Object success > .\mailResult.txt
 
-$EmailTo = "aeroadithiyan3@gmail.com "  
-    $EmailFrom = "adithiyan@minusculetechnologies.com" 
-    $Subject = "Test Powershell"  
-    $SMTPServer = "smtp.gmail.com" 
-    $SMTPMessage = New-Object System.Net.Mail.MailMessage($EmailFrom,$EmailTo,$Subject,$Body)
-    $SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, 587) 
-    $SMTPClient.EnableSsl = $true 
-    $SMTPClient.Credentials = New-Object System.Net.NetworkCredential("adithiyan@minusculetechnologies.com", "tfveajwjepitouum")
-    $SMTPClient.Send($SMTPMessage)
+$style = "<style>BODY{font-family: Arial; font-size: 13pt;}"
+$style = $style + "TABLE{border: 1px solid black; border-collapse: collapse;}"
+$style = $style + "TH{border: 1px solid black; background: #dddddd; padding: 5px; }"
+$style = $style + "TD{border: 1px solid black; padding: 5px; }"
+$style = $style + "</style>"
+
+$to      =  "aeroadithiyan3@gmail.com"
+$from    =  "adithiyan@minusculetechnologies.com"
+$subject =  "Test Powershell"
+$body    =  $JSONResult | Select-Object @{Name="Component Type";Expression={$_.componentType}}, @{Name="File Name";Expression={$_.fileName}}, @{Name="Full Name";Expression={$_.fullName}}, @{Name="Status";Expression={$_.success}} | Sort-Object success | ConvertTo-Html -Head $style 
+         
+
+$mail = New-Object System.Net.Mail.Mailmessage $from, $to, $subject, $body
+$mail.IsBodyHTML=$true
+$server = "smtp.gmail.com"
+$port   = 25
+$smtp = New-Object System.Net.Mail.SMTPClient $server,$port
+$smtp.EnableSsl = $true
+$smtp.Credentials = New-Object System.Net.NetworkCredential("adithiyan@minusculetechnologies.com", "tfveajwjepitouum")
+
+$smtp.send($mail)
